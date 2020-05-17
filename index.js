363bf44148be6e1a184a20ce9ec71973b1e83b4f
Dana Kamaran <dana.kamaran1996@gmail.com>
2020-05-17 14:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 let pets=["Milo", "Otis", "Garfield"];
-const append= pets.push('Odie');
+let append= pets.push('Odie');
 const prepend= pets.unshift("Odie");
 const removeLast= pets.pop();
 const removeFirst= pets.shift();
